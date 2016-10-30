8a1362282edfb813a0e154596c74ae6d43b01ed3
Jaime Valdivia <jaivaldivia@gmail.com>
2016-10-29 22:20:02 -0500

    Auth0 middleware added

--- server.js
+++ server.js
@@ -9,6 +9,83 @@ var pending = require('./data/pending');
 
 var app = express();
 
+var jwtCheck = jwt({
+  secret: rsaValidation(),
+  algorithms: ['RS256'],
+  issuer: "https://jaimevaldivia.auth0.com/",
+  audience: 'http://movieanalyst.com'
+});
+
+// Enable the use of the jwtCheck middleware in all of our routes
+app.use(jwtCheck);
+
+var guard = function(req, res, next){
+  var permissions, i;
+  // we’ll use a case switch statement on the route requested
+  switch(req.path){
+    // if the request is for movie reviews we’ll check to see if the token has general scope
+    case '/movies' : {
+      permissions = ['general'];
+      for(i = 0; i < permissions.length; i++){
+        if(req.user.scope.includes(permissions[i])){
+          next();
+        } else {
+          res.send(403, {message:'Forbidden'});
+        }
+      }
+      break;
+    }
+    // Same for the reviewers
+    case '/reviewers': {
+      permissions = ['general'];
+      for(i = 0; i < permissions.length; i++){
+        if(req.user.scope.includes(permissions[i])){
+          next();
+        } else {
+          res.send(403, {message:'Forbidden'});
+        }
+      }
+      break;
+    }
+    // Same for publications
+    case '/publications': {
+      permissions = ['general'];
+      for(i = 0; i < permissions.length; i++){
+        if(req.user.scope.includes(permissions[i])){
+          next();
+        } else {
+          res.send(403, {message:'Forbidden'});
+        }
+      }
+      break;
+    }
+    // For the pending route, we’ll check to make sure the token has the scope of admin before returning the results.
+    case '/pending': {
+      permissions = ['admin'];
+      console.log(req.user.scope);
+      for(i = 0; i < permissions.length; i++){
+        if(req.user.scope.includes(permissions[i])){
+          next();
+        } else {
+          res.send(403, {message:'Forbidden'});
+        }
+      }
+      break;
+    }
+  }
+};
+
+// existing app.use middleware
+
+app.use(guard);
+
+// If we do not get the correct credentials, we’ll return an appropriate message
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({message:'Missing or invalid token'});
+  }
+});
+
 app.get('/movies', function(req, res) {
   res.json(movies);
 });
