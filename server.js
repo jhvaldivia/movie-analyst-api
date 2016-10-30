3263eeb1fdc7f89a77b57635e18fffaab993ac0a
Jaime Valdivia <jaivaldivia@gmail.com>
2016-10-29 22:03:00 -0500

    Adding authors, publications and pending GET endpoints

--- server.js
+++ server.js
@@ -3,11 +3,26 @@ var jwt = require('express-jwt');
 var rsaValidation = require('auth0-api-jwt-rsa-validation');
 
 var movies = require('./data/movies');
+var authors = require('./data/authors');
+var publications = require('./data/publications');
+var pending = require('./data/pending');
 
 var app = express();
 
 app.get('/movies', function(req, res) {
   res.json(movies);
+});
+
+app.get('/authors', function(req, res) {
+  res.json(authors);
+});
+
+app.get('/publications', function(req, res) {
+  res.json(publications);
+});
+
+app.get('/pending', function(req, res) {
+  res.json(pending);
 });
 
 app.listen(3000);
